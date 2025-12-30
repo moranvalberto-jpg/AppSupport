8e11fa03d32a0ebc65f097d5f62bcfc5ad0eab65
AlbertoM <moranv.alberto@gmail.com>
2025-12-30 14:29:22 -0600

    modificando database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,21 +1,12 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  uri: process.env.MYSQL_PUBLIC_URL,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  connectTimeout: 10000
-});
-
-connection.getConnection((err) => {
-  if (err) {
-    console.error("❌ Error al conectar a MySQL:", err);
-  } else {
-    console.log("✅ Conectado a MySQL en Railway");
-  }
+  connectTimeout: 10000,
 });
